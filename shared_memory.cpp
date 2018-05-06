ce2ca44dcb94aba4583980ea39fab32b2184a99c
Matheus Miranda <matheusmirandalacerda@gmail.com>
2018-05-06 14:24:21 -0300

    Add logic to do communication using shared memory
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

---
+++ shared_memory.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 1024  /* make it a 1K shared memory segment */
+
+key_t key;
+int shmid;
+char *data;
+int mode;
+
+void attach_memory(){
+  /* make the key: */
+  if ((key = ftok("/home/miranda/Documents/nono_semestre/PI2/software_de_controle/hello.txt", 'R')) == -1) /*Here the file must exist */ 
+  {
+    perror("ftok");
+    exit(1);
+  }
+
+  /*  create the segment: */
+  if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1) {
+    perror("shmget");
+    exit(1);
+  }
+
+  /* attach to the segment to get a pointer to it: */
+  data = (char*)shmat(shmid, (void *)0, 0);
+  if (data == (char *)(-1)) {
+    perror("shmat");
+    exit(1);
+  }
+
+  ///* read or modify the segment, based on the command line: */
+  //if (argc == 2) {
+  //  printf("writing to segment: \"%s\"\n", argv[1]);
+  //  strncpy(data, argv[1], SHM_SIZE);
+  //} else
+  //  printf("segment contains: \"%s\"\n", data);
+
+}
+
+void detach_memory(){
+  /* detach from the segment: */
+  if (shmdt(data) == -1) {
+    perror("shmdt");
+    exit(1);
+  }
+}
--- source/main.cpp
+++ source/main.cpp
@@ -1,112 +1,114 @@
 #include "../include/common.hpp"
 #include "../include/player.hpp"
 #include "../include/interface.hpp"
-#include "../source/socket_connection.c"
+#include "../shared_memory.cpp"
 
 using namespace std;
 
 string get_status_of_information(SDL_Event e, string status_of_information){
-    if( e.key.keysym.sym == SDLK_s )
-    {
-       status_of_information = "11";
-    }
-    else if( e.key.keysym.sym == SDLK_p )
-    {
-       status_of_information =  status_of_information == "12" ? "12" : "13";
-    }
-    else if( e.key.keysym.sym == SDLK_c )
-    {
-       status_of_information = "14";
-    }
-	
-	if( e.key.keysym.sym == SDLK_f )
-	{
-		status_of_information = "32";
-	}
-	else if( e.key.keysym.sym == SDLK_q )
-	{
-		status_of_information = "33";
-	}
-	else if( e.key.keysym.sym == SDLK_w )
-	{
-		status_of_information = "31";
-	}
-	else if( e.key.keysym.sym == SDLK_e )
-	{
-		status_of_information = "30";
-	}
+  if( e.key.keysym.sym == SDLK_s )
+  {
+    status_of_information = "11";
+  }
+  else if( e.key.keysym.sym == SDLK_p )
+  {
+    status_of_information =  status_of_information == "12" ? "12" : "13";
+  }
+  else if( e.key.keysym.sym == SDLK_c )
+  {
+    status_of_information = "14";
+  }
 
-    return status_of_information;
+  if( e.key.keysym.sym == SDLK_f )
+  {
+    status_of_information = "32";
+  }
+  else if( e.key.keysym.sym == SDLK_q )
+  {
+    status_of_information = "33";
+  }
+  else if( e.key.keysym.sym == SDLK_w )
+  {
+    status_of_information = "31";
+  }
+  else if( e.key.keysym.sym == SDLK_e )
+  {
+    status_of_information = "30";
+  }
+
+  return status_of_information;
 }
 
 int main( int argc, char* args[] )
 {
-	Interface interface;
-	
-	if( interface.initInterface() )
-	{
-		bool quit = false;
-		SDL_Event e;
-	
-		Player lightPlayer, darkPlayer;
+  Interface interface;
 
-		pair<Player, Player> players (lightPlayer, darkPlayer);
-		pair<bool, string> endGame (false, "");
+  if( interface.initInterface() )
+  {
+    bool quit = false;
+    SDL_Event e;
 
-        string information = "";
-		string status_of_information = "2Welcome to chess";
-        string status_of_information_color_background = "";
-		
-		init_server();
-		while( !quit )
-		{			
-			bool el = SDL_PollEvent( &e )!=0;
-			if( el, e.type == SDL_QUIT )
-            {
-                std::cout << "\nSaiu no quit" << '\n';
-                quit = true;
-            }
-            else
-            {	
-				if(read(SD,rec_buff,300)!=0){
-					string a(rec_buff);
-					status_of_information = a;
-					cout << status_of_information << " - " << endl;
-				}
-				//while( SDL_PollEvent( &e ) != 0){
-					status_of_information = get_status_of_information(e, status_of_information);
-					if (status_of_information[0] == '3')
-					{
-						status_of_information_color_background = status_of_information;
-					}
-					else if (status_of_information[3] == '2')
-					{
-						information = status_of_information.erase(0,1);
-					}
-					else if (status_of_information[0]=='1')
-					{
-						players = interface.controlTime(status_of_information, players, &interface);
-					}
-				//}
-			} 
-				
-			interface.updateElements( players );
-			
-			endGame = interface.isGameOver(players);
-            if(endGame.second != "")
-            {
-                information = endGame.second;
-				players = interface.controlTime("15", players, &interface);
-            }
+    Player lightPlayer, darkPlayer;
 
-			interface.setInformation( information );
-			interface.drawBackgroundInterface( status_of_information );
-			
-			interface.renderElements();
-			SDL_RenderPresent( interface.gRenderer );
-		}
-	}
-	interface.close();
+    pair<Player, Player> players (lightPlayer, darkPlayer);
+    pair<bool, string> endGame (false, "");
 
-	return 0;
+    string information = "";
+    string status_of_information = "2Welcome to chess";
+    string status_of_information_color_background = "";
+
+    attach_memory();
+    while( !quit )
+    {			
+      bool el = SDL_PollEvent( &e )!=0;
+      if( el, e.type == SDL_QUIT )
+      {
+        std::cout << "\nSaiu no quit" << '\n';
+        quit = true;
+      }
+      else
+      {	
+        string a(data);        
+        if(a != "None"){
+          status_of_information = a;
+          strncpy(data, "None", SHM_SIZE);
+        }
+
+        //while( SDL_PollEvent( &e ) != 0){
+        status_of_information = get_status_of_information(e, status_of_information);
+        if (status_of_information[0] == '3')
+        {
+          status_of_information_color_background = status_of_information;
+        }
+        else if (status_of_information[3] == '2')
+        {
+          information = status_of_information.erase(0,1);
+        }
+        else if (status_of_information[0]=='1')
+        {
+          players = interface.controlTime(status_of_information, players, &interface);
+        }
+        //}
+      } 
+
+      interface.updateElements( players );
+
+      endGame = interface.isGameOver(players);
+      if(endGame.second != "")
+      {
+        information = endGame.second;
+        players = interface.controlTime("15", players, &interface);
+      }
+
+      interface.setInformation( information );
+      interface.drawBackgroundInterface( status_of_information );
+
+      interface.renderElements();
+      SDL_RenderPresent( interface.gRenderer );
+    }
+  }
+  detach_memory();
+  interface.close();
+
+  return 0;
 }
